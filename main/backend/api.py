4355f19932607f8dd63e83c52a6796a60b44f442
alifianadexe <alifianadexe@gmail.com>
2020-09-08 19:54:54 +0700

    initial commit frontend backend

---
+++ main/backend/api.py
@@ -0,0 +1,13 @@
+from backend.models import Lead
+from rest_framework import viewsets, permissions
+from .serializers import LeadSerializer
+
+# Lead Viewset
+
+class LeadViewSet(viewsets.ModelViewSet):
+    queryset = Lead.objects.all()
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+    serializer_class = LeadSerializer
+
