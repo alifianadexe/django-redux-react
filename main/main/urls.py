e67caa21913addab75020eee49c962b99046e343
alifianadexe <alifianadexe@gmail.com>
2020-09-09 22:23:50 +0700

    initial commit

--- main/main/urls.py
+++ main/main/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('backend.urls')),
+    path('', include('frontend.urls')),
+    path('', include('backend.urls'))
 ]
